47c878e207d3c72ed38f0cffdefba113cba901d3
Kangcheng Li <kangcheng@glodonusa.com>
2022-09-30 22:21:24 -0500

    Show courses

---
+++ src/components/CourseList.jsx
@@ -0,0 +1,8 @@
+
+const CourseList = ({courses}) => (
+  <div>
+    { Object.entries(courses).map(([key, value]) => <div>{value.term} CS {value.number}: {value.title}</div>) }
+  </div>
+);
+
+export default CourseList;

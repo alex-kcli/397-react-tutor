7f557932da4670fe7d780db0bac4285bf4fea8ab
Kangcheng Li <kangcheng@glodonusa.com>
2022-10-03 23:46:22 -0500

    term filter

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Banner from "./components/Banner";
 import CourseList from "./components/CourseList";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useJsonQuery } from './utilities/fetch';
+import MenuPage from './components/MenuPage';
 
 const Main = () => {
   const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
@@ -16,7 +17,8 @@ const Main = () => {
   return (
     <div>
       <Banner title={data.title}/>
-      <CourseList courses={data.courses}/>
+      <MenuPage courses={data.courses}/>
+      {/* <CourseList courses={data.courses}/> */}
     </div>
   );
 }
---
+++ src/components/MenuPage.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import CourseList from "./CourseList";
+
+const terms = {
+  Fall: 'Fall',
+  Winter: 'Winter',
+  Spring: 'Spring'
+};
+
+const MenuButton = ({term, selection, setSelection}) => (
+  <div>
+    <input type="radio" id={term} className="btn-check" checked={term === selection} autoComplete="off"
+      onChange={() => setSelection(term)} />
+    <label className="btn btn-success mb-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </div>
+);
+
+const MenuSelector = ({selection, setSelection}) => (
+  <div className="btn-group">
+    { 
+      Object.keys(terms).map(term => <MenuButton key={term} term={term} selection={selection} setSelection={setSelection} />)
+    }
+  </div>
+);
+
+const MenuPage = ({courses}) => {
+  const [selection, setSelection] = useState(() => Object.keys(terms)[0]);
+  return (
+    <div>
+      <MenuSelector selection={selection} setSelection={setSelection} />
+      <CourseList courses={Object.values(courses).filter(course => course.term === selection)}/>
+    </div>
+  );
+}
+
+export default MenuPage;
